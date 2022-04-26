b00f31eb37f3e68c394a621f712f5bb4288a8eb8
piotr <p.sztefko@gmail.com>
2022-04-26 15:41:42 +0200

    implemented correct way of counting values

--- main.py
+++ main.py
@@ -1,28 +1,50 @@
 import csv
+from typing import List
+from math import log2
 
 
-def load_data(path):
-    list_of_rows = []
-    with open(path, newline='') as file:
-        csv_reader = csv.reader(file, delimiter=',')
+def load_data(path: str) -> List[List[int]]:
+    """ "GIT"""
+    conditional_attributes = []
+    with open(path, newline="") as file:
+        csv_reader = csv.reader(file, delimiter=",")
         for row in csv_reader:
-            list_of_rows.append(row)
-            #print(row)
+            conditional_attributes.append(row)
 
-    return list_of_rows
-
-
-def identify_and_count_unique_classes(list_of_rows):
-    dict = {}
-    for row in list_of_rows:
-        for char in row:
-            if char in dict:
-                dict[char] += 1
-            else:
-                dict[char] = 0
-    return dict
+    return conditional_attributes
 
 
-list_of_rows = load_data('test.txt')
+def extract_columns(conditional_attributes: List[List[int]]) -> List[List[int]]:
+    """GIT"""
+    columns = []
+    for column_index in range(len(conditional_attributes[0])):
+        column = []
+        for row_index in range(len(conditional_attributes)):
+            column.append(conditional_attributes[row_index][column_index])
+        columns.append(column)
 
-print(identify_and_count_unique_classes(list_of_rows))
+    return columns
+
+
+def create_dictionary_of_values(column: List[int]) -> dict:
+    """GIT"""
+    dictionary_of_values = {}
+
+    for attribute in column:
+        if attribute in dictionary_of_values:
+            dictionary_of_values[attribute] += 1
+        else:
+            dictionary_of_values[attribute] = 0
+
+    return dictionary_of_values
+
+def get_class_occurances(columns):
+    return [create_dictionary_of_values(column) for column in columns]
+
+conditional_attributes = load_data("test2.txt")
+
+columns = extract_columns(conditional_attributes)
+
+
+list_of_dicts = get_class_occurances(columns)
+print(list_of_dicts)
