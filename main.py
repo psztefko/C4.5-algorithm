4de5cb6461d6ad5a7b8fb72b47d1fc47be304b9a
piotr <p.sztefko@gmail.com>
2022-04-26 21:13:30 +0200

    implemented probability counting

--- main.py
+++ main.py
@@ -34,17 +34,33 @@ def create_dictionary_of_values(column: List[int]) -> dict:
         if attribute in dictionary_of_values:
             dictionary_of_values[attribute] += 1
         else:
-            dictionary_of_values[attribute] = 0
+            dictionary_of_values[attribute] = 1
 
     return dictionary_of_values
 
-def get_class_occurances(columns):
+
+def get_class_occurrences(columns):
+    """GIT"""
     return [create_dictionary_of_values(column) for column in columns]
 
-conditional_attributes = load_data("test2.txt")
 
+def count_probability(conditional_attribute_dict: dict, column_length: int) -> List[float]:
+    return [element / column_length for element in conditional_attribute_dict.values()]
+
+
+conditional_attributes = load_data("gielda.txt")
+
+# convert list of rows to list of columns
 columns = extract_columns(conditional_attributes)
 
+decisional_attribute = columns[-1]
+columns = columns[:-1]
 
-list_of_dicts = get_class_occurances(columns)
-print(list_of_dicts)
+# get dict of values and their occurrences for each attribute
+list_of_dicts = get_class_occurrences(columns)
+
+list_of_probabilities = []
+for dictionary in list_of_dicts:
+    list_of_probabilities.append(count_probability((dictionary), len(columns[0])))
+
+
