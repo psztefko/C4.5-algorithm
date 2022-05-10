0a7727f10aca3909c16b86c0503b014279466420
piotr <p.sztefko@gmail.com>
2022-05-10 09:27:53 +0200

    dict creation

--- src/Tree.py
+++ src/Tree.py
@@ -11,19 +11,23 @@ class Node:
     branch_label: str  # wartosc atrybutu z ktorego zostala stworzona typu old, mid, new
     table: Table  # tablica
     children: List[Node]
+    indent: int
+    parent_label: str
 
-    # def __init__(self, label: str, branch_label: str, table: Table, children: List[Node]):
-    #     self.label = label
-    #     self.branch_label = branch_label
-    #     self.table = table
-    #     self.children = []
+    def __init__(self, label: str, branch_label: str, table: Table, children: List[Node], indent: int):
+        self.label = label
+        self.branch_label = branch_label
+        self.table = table
+        self.children = children
+        self.indent = indent + 1
 
     def __str__(self):
         return {
-            'label: ': self.label,
-            'branch label: ': self.branch_label,
-            'table: ': self.table.table,
-            'children: ': [children.__str__() for children in self.children]
+            'Label: ': self.label,
+            'Branch label: ': self.branch_label,
+            #'Table: ': self.table.table,
+            'Children: ': [children.__str__() for children in self.children],
+            'Indent: ': self.indent
         }
 
     # def __init__(self, branch_label: str, decision_table: Table):
@@ -36,29 +40,24 @@ class Node:
     #     self.branch_label = branch_label
     #     self.children = []
 
-    def add_child(self, child: Node):
-        self.children.append(child)
+    def get_children(self, children):
+        return children
 
-    # def get_children(self, children):
-    #     return children
-    #
-    # def get_table(self):
-    #     return self.decision_table
-    #
-    # def set_label(self, label: str):
-    #     self.label = label
-    #
-    # def get_label(self):
-    #     return self.label
-    #
-    # def set_branch_label(self, branch_label: str):
-    #     self.branch_label = branch_label
-    #
-    # def get_branch_label(self):
-    #     return self.branch_label
-    #
-    # def is_leaf(self) -> bool:
-    #     return True if self.children else False
-    #
-    # def is_root(self) -> bool:
-    #     return self.branch_label == 1
+    def get_table(self):
+        return self.decision_table
+
+    def get_label(self):
+        return self.label
+
+    def get_branch_label(self):
+        return self.branch_label
+
+    def get_decision_attr(self):
+        return self.table.table[0][-1]
+
+    def is_leaf(self) -> bool:
+        return False if len(self.children) == 0 else True
+
+    def is_root(self) -> bool:
+        return self.branch_label == 'root'
+
